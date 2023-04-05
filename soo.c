e6a6b2162617d87b8d99dc82604a09609916aae0
30120 <>
2023-04-05 01:13:56 +0000

    이진법과 컴퓨터 언어

--- soo.c
+++ soo.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-int main(avoid)
-{int a, b;             //
-int c=30, d=40;
-a=10;
-b=20;
-printf("%d %d \n", a, b);
-printf("%d %d \n", c, d);
-}
+// #include <stdio.h>
+// int main(avoid)
+// {int a, b;             //
+// int c=30, d=40;
+// a=10;
+// b=20;
+// printf("%d %d \n", a, b);
+// printf("%d %d \n", c, d);
+// }
